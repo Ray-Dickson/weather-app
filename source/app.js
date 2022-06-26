e966c7d12d727a6cce663e2d7be3638a09da1184
RayDada <tenderray10@gmail.com>
2022-06-26 07:57:37 +0000

    Setup app for heroku

--- source/app.js
+++ source/app.js
@@ -7,6 +7,7 @@ const forecast = require("./utills/forecast")
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 //Defining path for express configuration
@@ -115,6 +116,6 @@ app.get("*", (req, res)=>{
 //      location: 'Philadelphia'
 //  })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
